9458e432536641a04507c7c94932fbd0258ed09d
Oliver Clarke <oliver.clarke.crgs@gmail.com>
2025-11-28 13:06:43 +0000

    fix: bad tests removed

--- test_stuff.py
+++ test_stuff.py
@@ -3,14 +3,15 @@ import main
 def test_func1():
     assert main.func1(3) == 9
     #x = 1/0
-    raise Exception("Deliberate Exception for testing")
+    #raise Exception("Deliberate Exception for testing")
     assert main.func1(-4) == 16
     assert main.func1(0) == 0
-    assert main.func1(20) == 5  # This will fail
+    #assert main.func1(20) == 5  # This will fail
 
 def test_func2():
     assert main.func2("Testing") == "Testin"
     assert main.func2("A") == ""
 
 def test_bad_func1():
-    assert main.func1(2) == 5  # This will fail
+    #assert main.func1(2) == 5  # This will fail
+    pass
